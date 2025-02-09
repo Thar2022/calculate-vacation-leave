fe63d18d9a020d729ac779006c89bfd2af9a5771
suphakorn-aileen <suphakorn@aileensolutions.com>
2025-02-09 17:15:44 +0700

    test

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,7 @@
-// import LeaveRequestModel from 'LeaveRequestModel.js';
-// import { displayDateThai } from 'utils.js';
+import LeaveRequestModel from 'LeaveRequestModel.js';
+import { displayDateThai } from 'utils.js';
 console.log("ee",document.getElementById("result"))
-document.getElementById("result").value = "2"
+document.getElementById("result").value = "22mm"
 
 document.addEventListener("DOMContentLoaded", function () {
     const summitButton = document.getElementById("summit");
